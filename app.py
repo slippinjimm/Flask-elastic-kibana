4ad6a354a3ca66700d221e9c04715c89d56faecc
Brkcr <118850232+slippinjimm@users.noreply.github.com>
2024-05-22 12:38:59 +0400

    change add.py and added requriments

--- app.py
+++ app.py
@@ -1,44 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask
 from elasticsearch import Elasticsearch
 
 
 app=Flask(__name__)
 client = Elasticsearch("http://localhost:9200")
 
-@app.route('/')
-def index():
-    mappings = {
-        "properties": {
-            "foo": {"type": "text"},
-            "bar": {
-                "type": "text",
-                "fields": {
-                    "keyword": {
-                        "type": "keyword",
-                        "ignore_above": 256,
-                    }
-                },
-            },
-        }
-    }
-    client.indices.create(index="my_index", mappings=mappings)
-    client.index(
-        index="my_index",
-        id="my_document_id",
-        document={
-            "foo": "foo",
-            "bar": "bar",
-        },
-    )
-    client.update(
-    index="my_index",
-    id="my_document_id",
-    doc={
-        "foo": "bar",
-        "new_field": "new value",
-    },
-)
-    f=client.get(index="my_index", id="my_document_id")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+data = {
+    "days_of_week": [
+        {"id": 1, "name": "Понедельник"},
+        {"id": 2, "name": "Вторник"},
+        {"id": 3, "name": "Среда"},
+        {"id": 4, "name": "Четверг"},
+        {"id": 5, "name": "Пятница"},
+        {"id": 6, "name": "Суббота"},
+        {"id": 7, "name": "Воскресенье"},
+    ]
+}
 
 
-    return render_template('index.html',messages=f)
+
+@app.route('/', methods=['GET'])
+def get_users():
+    return jsonify(data["days_of_week"])
+
+
+
+if __name__ == '__main__':
+    app.run()
